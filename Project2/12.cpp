b2df85cd8dbd0a7b1b679eec766b62abd596bbb3
student4 <stud4@STUDENT4>
2025-10-13 18:13:25 +0700

    test

--- Project2/12.cpp
+++ Project2/12.cpp
@@ -30,8 +30,65 @@ void test() {
 
 }
 
+
+void game()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        int bot = rand() % 3;
+        std::string words[3]{ "rock", "scissors", "paper" };
+        std::cout << words[bot] << std::endl;
+        std::string uword;
+        std::cin >> uword;
+
+        if (uword == "rock" && words[bot] == "scissors" ||
+            uword == "scissors" && words[bot] == "paper" ||
+            uword == "paper" && words[bot] == "rock")
+        {
+            std::cout << "WIN" << std::endl;
+        }
+        else if (uword == words[bot])
+        {
+            std::cout << "DRAW" << std::endl;
+        }
+        else
+        {
+            std::cout << "LOSE" << std::endl;
+        }
+    }
+}
+
+
+
+
+void suefa()
+{
+
+    int bot = rand() % 3 + 1;
+    int a = 0;
+    std::cout << bot << std::endl;
+    std::cin >> a;
+
+    if (a == 1 && bot == 2 ||
+        a == 2 && bot == 3 ||
+        a == 3 && bot == 1)
+    {
+        std::cout << "WIN" << std::endl;
+    }
+    else if (a == bot)
+    {
+        std::cout << "DRAW" << std::endl;
+    }
+    else
+    {
+        std::cout << "LOSE" << std::endl;
+    }
+}
+
 int main()
 {
     srand(time(NULL));
-    test();
+    //test();
+    //suefa();
+    game();
 }
