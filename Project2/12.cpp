745944327a1cf2bff19a33a8695b90f45f31c238
student4 <stud4@STUDENT4>
2025-10-13 16:27:52 +0700

    test

--- Project2/12.cpp
+++ Project2/12.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 
-int main()
-{
-    srand(time(NULL));
-    int bot = rand() % 5 + 1;
+void test() {
+
+    int bot = 1 + rand() % 3;
     int a = 0;
-    while (bot != a)
+    int i = 0;
+    while (i < 5)
     {
+        std::cout << bot << std::endl;
         std::cin >> a;
         if (a == bot)
         {
             std::cout << "WIN" << std::endl;
-            bot = rand() % 5 + 1;
-            while (bot == a)
+            bot = 1 + rand() % 3;
+            /*while (bot == a)
             {
                 bot = rand() % 5 + 1;
-            }
+            }*/
+            i++;
         }
         else
         {
             std::cout << "LOSE" << std::endl;
         }
+
     }
+
+
+
+}
+
+int main()
+{
+    srand(time(NULL));
+    test();
 }
