913cd36df36f766ef1dba39a8a67d234b1abe648
student4 <stud4@STUDENT4>
2025-10-17 17:16:14 +0700

    кошелек отнималось колво товаров

--- Project2/12.cpp
+++ Project2/12.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-void test() {
+struct product_ {
+    string name;
+    int count;
+    int price;
 
-    int bot = 1 + rand() % 3;
-    int a = 0;
-    int i = 0;
-    while (i < 5)
-    {
-        std::cout << bot << std::endl;
-        std::cin >> a;
-        if (a == bot)
+    void test() {
+
+        int bot = 1 + rand() % 3;
+        int a = 0;
+        int i = 0;
+        while (i < 5)
         {
-            std::cout << "WIN" << std::endl;
-            bot = 1 + rand() % 3;
-            /*while (bot == a)
+            std::cout << bot << std::endl;
+            std::cin >> a;
+            if (a == bot)
             {
-                bot = rand() % 5 + 1;
-            }*/
-            i++;
-        }
-        else
-        {
-            std::cout << "LOSE" << std::endl;
+                std::cout << "WIN" << std::endl;
+                bot = 1 + rand() % 3;
+                /*while (bot == a)
+                {
+                    bot = rand() % 5 + 1;
+                }*/
+                i++;
+            }
+            else
+            {
+                std::cout << "LOSE" << std::endl;
+            }
+
         }
 
     }
 
 
+};
 
-}
+product_ l;
+
 
 
 void game()
@@ -85,10 +96,144 @@ void suefa()
     }
 }
 
+int numbers[5]{ 7,5,-1,3,-7 };
+int sisi = size(numbers);
+
+void otr()
+{
+
+
+    for (int i = 0; i < std::size(numbers); i++)
+    {
+        if (numbers[i] < 0)
+        {
+            std::cout << numbers[i] << std::endl;
+        }
+
+    }
+}
+
+
+void inv()
+{
+    //int numbers[5]{ 7,5,-1,3,-7 };
+    for (int i = 0; i < std::size(numbers); i++)
+    {
+        numbers[i] = -numbers[i];
+        std::cout << numbers[i] << std::endl;
+    }
+}
+
+
+void repl1()
+{
+    //int numbers[5]{ 7,5,-1,3,-7 };
+    for (int i = 0; i < std::size(numbers); i++)
+    {
+        numbers[i] = 1;
+        std::cout << numbers[i] << std::endl;
+    }
+}
+
+
+void repl()
+{
+    //int numbers[5]{ 7,5,-1,3,-7 };
+    for (int i = 0; i < std::size(numbers); i++)
+    {
+        numbers[i] = i;
+        std::cout << numbers[i] << std::endl;
+    }
+}
+
+
+void bsort()
+{
+    
+
+    for (int i = 0; i < sisi - 1; i++)
+    {
+        for (int j = 0; j < sisi - 1 - i; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                //int t = numbers[j];
+                std::swap(numbers[j], numbers[j + 1]);
+                //numbers[j] = numbers[j + 1];
+                //numbers[j + 1] = t;
+
+            }
+        }
+    }
+
+    for (int k = 0; k < sisi; k++)
+    {
+        std::cout << numbers[k] << std::endl;
+    }
+}
+
+
+
 int main()
 {
-    srand(time(NULL));
-    //test();
-    //suefa();
-    game();
+    vector<product_> shop;
+    shop.push_back({ "banana", 2, 110 });
+    shop.push_back({ "apple", 20, 150 });
+    shop.push_back({ "tomato", 15, 200 });
+    shop.push_back({ "chees", 0, 300 });
+    string chname;
+    int chcount;
+    int chmoney;
+    int hn = 0;
+    int id = 0;
+    double a = 0;
+
+    while (true) {
+
+        for (int i = 0; i < shop.size(); i++) {
+
+
+            cout << shop[i].name << "\t" << shop[i].count << "\t" << shop[i].price << endl;
+
+
+        }
+
+        cout << "\nchoose name\n";
+        cin >> chname;
+        for (int i = 0; i < 4; i++)
+        {
+            if (chname == shop[i].name)
+            {
+                id = i;
+            }
+        }
+        cout << "\nchoose number of products\n";
+        cin >> chcount;
+        cout << "\n write your budget\n";
+        cin >> chmoney;
+        a = chmoney - chcount * shop[id].price;
+
+        if (shop[id].count < chcount)
+        {
+            cout << "\nnot enough products in the shop\navailaible: " << shop[id].count;
+        }
+        else if (a >= 0)
+        {
+            cout << "\nyour change: " << a << endl;
+        }
+        else if (a < 0)
+        {
+            cout << "\nnot enough: \n" << abs(a) << endl;
+        }
+        
+
+
+       
+    }
+    
+
+    //srand(time(NULL));
+    //l.test();
+    //l.name = "banana";
+    //cout << l.name;
 }
